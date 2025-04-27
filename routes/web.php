6b3ec4b6c2bd57dbe8512495b757af60dbbc0bb2
yasvxz <huma.kias89@gmail.com>
2025-04-27 18:27:23 +0700

    PENGERJAAN TP MODUL 4 PAW

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// 1. Tambahkan route untuk menampilkan daftar pengguna
+Route::get('/users/index', [UserController::class, 'index'])->name('users.index');
+
+// 2. Tambahkan route untuk menampilkan form tambah pengguna
+Route::get('/create', [UserController::class, 'create'])->name('users.create');
+
+
+// 3. Tambahkan route untuk menyimpan pengguna baru
+Route::post('/simpan', [UserController::class, 'store'])->name('users.store');
+
+// 4. Tambahkan route untuk menampilkan form edit pengguna
+Route::get('/edit/{id}', [UserController::class, 'edit'])->name('users.edit');
+
+
+// 5. Tambahkan route untuk menyimpan perubahan pengguna
+Route::put('/update/{id}', [UserController::class, 'update'])->name('users.update');
+
+
+// 6. Tambahkan route untuk menghapus pengguna
+Route::delete('/hapus/{id}', [UserController::class, 'destroy'])->name('users.destroy');
